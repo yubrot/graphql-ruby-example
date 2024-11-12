91693ab80ea0873bc4afd98f4320b9ca29211b7f
yubrot <yubrot@gmail.com>
2024-11-12 19:11:36 +0900

    Default `--require rails_helper` on .rspec

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 RSpec.describe Reaction do
   let(:reaction) { build_stubbed(:reaction) }
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,11 +6,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
-# that will avoid rails generators crashing because migrations haven't been run yet
-# return unless Rails.env.test?
+
+return unless Rails.env.test?
+
 require "rspec/rails"
-# Add additional requires below this line. Rails is not loaded until this point!
+require "json_expressions/rspec"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
