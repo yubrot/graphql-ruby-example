798d9de35c188a32f6a2421116c33440840f9c22
yubrot <yubrot@gmail.com>
2024-11-13 00:23:30 +0900

    Separate Query spec into Resolvers specs

---
+++ spec/graphql/gre/resolvers/node_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe Gre::Resolvers::Node, type: :request do
+  subject do
+    post("/graphql", params: { query:, variables: })
+    response
+  end
+
+  let(:query) do
+    <<~GRAPHQL
+      query($id: ID!) {
+        node(id: $id) {
+          id
+          __typename
+        }
+      }
+    GRAPHQL
+  end
+  let(:variables) { { id: } }
+
+  context "when the object associated with the id does not exist" do
+    let(:id) { build_stubbed(:user).to_gid_param }
+
+    it "returns null" do
+      expect(subject).to have_attributes(
+        status: 200,
+        parsed_body: match_json_expression(data: { node: nil }),
+      )
+    end
+  end
+
+  context "when the object associated with the id exists" do
+    let(:id) { create(:user).to_gid_param }
+
+    it "returns the object" do
+      expect(subject).to have_attributes(
+        status: 200,
+        parsed_body: match_json_expression(data: { node: { __typename: "User", id: } }),
+      )
+    end
+  end
+end
